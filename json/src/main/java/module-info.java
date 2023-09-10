118804a1c8da96fc4a721a7a61e58241acb90fd1
刘升龙 <liushenglong_8597@outlook.com>
2023-09-10 18:18:03 +0800

    Update project jdk to 17 to use modular feature

---
+++ json/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+/**
+ * <p> </p>
+ *
+ * @author liushenglong_8597@outlook.com
+ * @since 2023/9/10
+ */
+module common.json {
+     exports cn.brk2outside.common.json;
+     requires common.lang;
+     requires com.fasterxml.jackson.core;
+     requires com.fasterxml.jackson.databind;
+     requires lombok;
+}
---
+++ lang/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * <p> </p>
+ *
+ * @author liushenglong_8597@outlook.com
+ * @since 2023/9/10
+ */
+module common.lang {
+    exports cn.brk2outside.common.lang.reflect;
+    exports cn.brk2outside.common.lang.type;
+}
